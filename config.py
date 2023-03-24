177a164b72d7692c2a80840b5428165abdc5f389
lucasOlivio <lucas27_olivio@hotmail.com>
2023-03-24 10:54:33 -0300

    First Commit

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+def can_build(env, platform): 
+    # Returns if its ok to build for the platform
+    # Will build for every platform
+    return True
+
+def configure(env):
+    pass
---
+++ register_types.h
@@ -0,0 +1,5 @@
+/* register_types.h */
+
+void register_summator_types();
+void unregister_summator_types();
+/* The word in the middle MUST be the same as the module folder name */
